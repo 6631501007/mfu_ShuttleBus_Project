8c7f91d1962f0e9b790660deb71fe7ef900bd359
LordCalcium <plaguebird42@gmail.com>
2026-05-19 14:14:50 +0700

    feat: integrate Tailwind CSS and add Livefeed view
    - Updated package.json to include Tailwind CSS dependencies and updated Vite version.
    
    - Added Livefeed route to the router.
    
    - Created Livefeed.vue component for live monitoring interface.
    
    - Configured Tailwind CSS in style.css and added postcss.config.js.
    
    - Added tailwind.config.js for Tailwind CSS configuration.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,14 @@ import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
 import History from '../views/History.vue'
 import Home from '../views/Home.vue'
+import Livefeed from '../views/Livefeed.vue'
 
 const routes = [
   { path: '/', component: Login },
   { path: '/dashboard', component: Dashboard },
   { path: '/history', component: History },
   { path: '/home', component: Home },
+  { path: '/livefeed', component: Livefeed},
 ]
 
 const router = createRouter({
